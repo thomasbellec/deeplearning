06270ab0066e80032db5017f58ae0e602cd5dced
thomasbellec <38011855+thomasbellec@users.noreply.github.com>
2019-03-28 19:24:27 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# Projet Analyse de Sentiment
+
+_Groupe 28 :_  
+- _Thomas Bellec_
+- _Louise Picard_
+- _Erwan de Kergaradec_
+- _Gabrielle Rappaport_
+
+### Objectif
+
+Ce projet a pour but de présenter une étude d'analyse de sentiments en français.
+Nous nous servons ici de deux datasets : FEEL et EMOTION IN TEXT.
+
+### Structure
+
+Ce projet se décompose de la manière suivante : 
+- code_projet
+    - data_science 
+        - autre_dataset.ipynb : jupyter notebook permettant de tester des modèles sur le dataset EMOTION IN TEXT.
+        - Dataset_Feel.ipynb : jupyter notebook permettant de tester des modèles sur le dataset FEEL.
+        - evaluate.py : script permettant d'évaluer le modèle choisi dans config.py sur le dataset de test
+        - learning.py : script permettant l'apprentissage d'un modèle
+        - predict.py : script permettant la prédiction d'une phrase à partir d'un modèle appris
+        - preprocessing.py : ensemble de traitement NLP permettant de traiter nos phrases
+        - utils_project.py : ensemble de fonctions utiles pour le traitement de nos données
+    - static : contient des images et des fichiers .css pour l'application web
+    - templates : contient les deux templates des pages html de l'application web
+    - config.py : fichier de configuration contenant les variables globales du projet
+    - main.py : fichier principal pour lancer l'application web
+    - slackbot.py : fichier principal pour lancer l'application slack
+- data
+    - Feel-fr : ensemble de fichiers contenant en particulier le dataset FEEL
+    - kaggle-traduction : ensemble de fichiers contenant en particulier le dataset EMOTION IN TEXT traduit
+    - modeles : dossier regroupant les fichiers de sauvegarde des apprentissages et vectorisations
+    - test_set.xlsx : dataset de test
+- requirements.txt : fichier contenant les modules nécessaires au projet
---
+++ slackbot.py
@@ -0,0 +1,87 @@
+import os
+import time
+import re
+from slackclient import SlackClient
+from code_project.data_science.predict import get_sentiment
+
+SLACK_BOT_TOKEN = 'xoxb-250735633280-566864170597-9nzs0ug33ULlQVJvsEt9wiAI'
+# instantiate Slack client
+slack_client = SlackClient(SLACK_BOT_TOKEN)
+# starterbot's user ID in Slack: value is assigned after the bot starts up
+starterbot_id = None
+
+# constants
+RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+EXAMPLE_COMMAND = "do"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+def parse_bot_commands(slack_events):
+    """
+        Parses a list of events coming from the Slack RTM API to find bot commands.
+        If a bot command is found, this function returns a tuple of command and channel.
+        If its not found, then this function returns None, None.
+    """
+    for event in slack_events:
+        if event["type"] == "message" and not "subtype" in event:
+            user_id, message = parse_direct_mention(event["text"])
+            if user_id == starterbot_id:
+                return message, event["channel"]
+    return None, None
+
+def parse_direct_mention(message_text):
+    """
+        Finds a direct mention (a mention that is at the beginning) in message text
+        and returns the user ID which was mentioned. If there is no direct mention, returns None
+    """
+    matches = re.search(MENTION_REGEX, message_text)
+    # the first group contains the username, the second group contains the remaining message
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def handle_command(command, channel):
+    """
+        Executes bot command if the command is known
+    """
+    # Default response is help text for the user
+    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+
+
+    # This is where you start to implement more commands!
+    print(command, type(command))
+    sentiment = get_sentiment(command)
+
+    if sentiment == "joy":
+        response = ":smiley:"
+    elif sentiment == "sadness":
+        response = ":disappointed_relieved:"
+    elif sentiment == "fear":
+        response = ":fearful:"
+    elif sentiment == "anger":
+        response = ":rage:"
+    elif sentiment == "surprise":
+        response = ":open_mouth:"
+    elif sentiment == "disgust":
+        response = ":nauseated_face:"
+    elif sentiment == "no_emotion":
+        response = ":neutral_face:"
+    else:
+        response = sentiment
+
+    # Sends the response back to the channel
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=response or default_response
+    )
+
+if __name__ == "__main__":
+    if slack_client.rtm_connect(with_team_state=False):
+        print("Starter Bot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        starterbot_id = slack_client.api_call("auth.test")["user_id"]
+        while True:
+            command, channel = parse_bot_commands(slack_client.rtm_read())
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
+    else:
+        print("Connection failed. Exception traceback printed above.")
